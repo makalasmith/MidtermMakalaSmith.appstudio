986e61b20b87a18d58df76c0ea8e3ed463506dde
Makala Smith <mms96354@creighton.edu>
2021-03-15 15:21:11 -0500

    not finished calculate

--- forms/calculateEC/calculateEC.js
+++ forms/calculateEC/calculateEC.js
@@ -2,14 +2,6 @@ function divideTwo(number1, number2){
   let answer = number1 % number2
   return answer
 }
-
-function calcAvgSquare (num1, num2, num3){
-  let sum = num1 + num2 + num3
-  let average = sum / 3
-  let squared = Math.pow(num1,2)
-  let answer = average * squared
-  return answer
-}
   
 let number1 = parseInt(prompt("What is your first number?"))
 let number2 = parseInt(prompt("What is your second number?"))
@@ -20,19 +12,11 @@ let yesNo = ""
 
 while (averageRemainder == false){
     yesNo = prompt("Do you want to calulate an average or a division remainder calculation? Enter: Average or Remainder?")
-    if (yesNo == "Average"){
-    } else
-      doneWithList = true {
+    if (yesNo == "Remainder"){
       let remainder = divideTwo(number1, number2)
       let division = Math.floor(number1/number2)
       console.log(`The result of dividing ${number1} by ${number2} is ${division} with a remainder of ${remainder}.`)
-      }
+    } else
+      doneWithList = true
 }
 
-
-let count = 0
-while (bucketList.length !== count){
-  let name = bucketList[count]
-  console.log(`${name}`)
-  count++
-}
