d6d80f42367ab4b1d506fdd62a01a8d9a2c7942e
Makala Smith <mms96354@creighton.edu>
2021-03-15 14:10:27 -0500

    finished animals

---
+++ forms/animals/animals.js
@@ -0,0 +1,11 @@
+let myAnimals = ["dog", "cat", "horse", "meerkat"]
+
+let newAnimal = prompt("Enter an animal to add to the list:")
+
+let newAnimalLowerCase = newAnimal.toLowerCase()
+
+myAnimals.push(newAnimalLowerCase)
+
+let myAnimalsReverse = myAnimals.reverse()
+
+console.log(`The last animal is a/an ${myAnimalsReverse[0]}`)
